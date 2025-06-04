12c39c885670e0b69d16054ef919d74966cc9e70
alhabast <alhabast@gmail.com>
2025-06-04 17:15:21 +0200

    Art pass menu
    art pass menu

--- scripts/bulb_cleaner_logic.gd
+++ scripts/bulb_cleaner_logic.gd
@@ -4,5 +4,6 @@ extends Node2D
 
 func _physics_process(_delta):
 	if tilemap.score >= 1:
+		get_node("winlabel").show()
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scripts/minigames/gardening/gardening_gamelogic.gd
+++ scripts/minigames/gardening/gardening_gamelogic.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 const winscore = 12
-var score = 0
+var score = 0 
 
 #checks if score is high enough to win
 func _process(_delta):
 	if score >= winscore:
+		get_node("winlabel").show()
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-
