7cd43ff6c0b98b4538d69b07890d679aa3200929
alhabast <alhabast@gmail.com>
2025-06-05 13:41:03 +0200

    made game work

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 
-func _on_exit_button_pressed() -> void:
-	get_tree().quit()
+#func _on_exit_button_pressed() -> void:
+	#get_tree().quit()
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_select_menu.tscn")
